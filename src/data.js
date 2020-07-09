d82901190f30be996614483f2eb6cf9ee76e4d98
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-09 15:07:08 +0100

    add folder with logos and change their path

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ const joblistings = [
   {
     id: 1,
     company: "Photosnap",
-    logo: "./images/photosnap.svg",
+    logo: "photosnap.svg",
     new: true,
     featured: true,
     position: "Senior Frontend Developer",
@@ -17,7 +17,7 @@ const joblistings = [
   {
     id: 2,
     company: "Manage",
-    logo: "./images/manage.svg",
+    logo: "manage.svg",
     new: true,
     featured: true,
     position: "Fullstack Developer",
@@ -32,7 +32,7 @@ const joblistings = [
   {
     id: 3,
     company: "Account",
-    logo: "./images/account.svg",
+    logo: "account.svg",
     new: true,
     featured: false,
     position: "Junior Frontend Developer",
@@ -47,7 +47,7 @@ const joblistings = [
   {
     id: 4,
     company: "MyHome",
-    logo: "./images/myhome.svg",
+    logo: "myhome.svg",
     new: false,
     featured: false,
     position: "Junior Frontend Developer",
@@ -62,7 +62,7 @@ const joblistings = [
   {
     id: 5,
     company: "Loop Studios",
-    logo: "./images/loop-studios.svg",
+    logo: "loop-studios.svg",
     new: false,
     featured: false,
     position: "Software Engineer",
@@ -77,7 +77,7 @@ const joblistings = [
   {
     id: 6,
     company: "FaceIt",
-    logo: "./images/faceit.svg",
+    logo: "faceit.svg",
     new: false,
     featured: false,
     position: "Junior Backend Developer",
@@ -92,7 +92,7 @@ const joblistings = [
   {
     id: 7,
     company: "Shortly",
-    logo: "./images/shortly.svg",
+    logo: "shortly.svg",
     new: false,
     featured: false,
     position: "Junior Developer",
@@ -107,7 +107,7 @@ const joblistings = [
   {
     id: 8,
     company: "Insure",
-    logo: "./images/insure.svg",
+    logo: "insure.svg",
     new: false,
     featured: false,
     position: "Junior Frontend Developer",
@@ -122,7 +122,7 @@ const joblistings = [
   {
     id: 9,
     company: "Eyecam Co.",
-    logo: "./images/eyecam-co.svg",
+    logo: "eyecam-co.svg",
     new: false,
     featured: false,
     position: "Full Stack Engineer",
@@ -137,7 +137,7 @@ const joblistings = [
   {
     id: 10,
     company: "The Air Filter Company",
-    logo: "./images/the-air-filter-company.svg",
+    logo: "the-air-filter-company.svg",
     new: false,
     featured: false,
     position: "Front-end Dev",
