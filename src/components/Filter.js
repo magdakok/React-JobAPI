394a9eaa792bd8b4997bbdcf1305b82622a42636
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-11 21:04:05 +0100

    use TechButton to display filter labels

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import TechButton from "./TechButton";
 
 const ButtonClear = styled.span`
   color: hsl(180, 29%, 50%);
@@ -10,27 +11,34 @@ const ButtonClear = styled.span`
   }
 `;
 
-function Filter(props) {
-  const Container = styled.div`
-    visibility: ${props.hidden ? "hidden" : "visible"};
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: white;
-    margin: 21px 7px -79px 7px;
-    padding: 21px 41px;
-    border-radius: 7px;
-    box-shadow: 5px 6px 25px -16px rgba(0, 0, 0, 0.3);
-    transform: translateY(-102px);
-  `;
+const ButtonsBox = styled.div`
+  display: flex;
+`;
 
+const Container = styled.div`
+  ${(props) => props.hidden && "visibility: hidden;"}
+  ${(props) => !props.hidden && "visibility: visible;"}
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  margin: 21px 7px -79px 7px;
+  padding: 21px 41px;
+  border-radius: 7px;
+  box-shadow: 5px 6px 25px -16px rgba(0, 0, 0, 0.3);
+  transform: translateY(-102px);
+`;
+
+function Filter(props) {
   return (
     <Container>
-      <div>
+      <ButtonsBox>
         {props.labels.map((l) => (
-          <span>{l}</span>
+          <TechButton text={l} id={`TB-${l}`}>
+            {l}
+          </TechButton>
         ))}
-      </div>
+      </ButtonsBox>
       <ButtonClear onClick={props.clear}>Clear</ButtonClear>
     </Container>
   );
