f7f3788eb532ea55cd8d5be339a7bbec25086ee8
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-12 20:45:34 +0100

    Updates

---
+++ precache-manifest.949408a839eb8842ffb3adbf6d46422b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e8aff50c8badfe68e351779ac9a092",
+    "url": "/React-JobAPI/index.html"
+  },
+  {
+    "revision": "721c880c128a80795511",
+    "url": "/React-JobAPI/static/css/main.f77f7699.chunk.css"
+  },
+  {
+    "revision": "83336b4ca2c9f6371d0b",
+    "url": "/React-JobAPI/static/js/2.52aea22b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-JobAPI/static/js/2.52aea22b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721c880c128a80795511",
+    "url": "/React-JobAPI/static/js/main.96b7c198.chunk.js"
+  },
+  {
+    "revision": "16ed0c909a6354e0e0de",
+    "url": "/React-JobAPI/static/js/runtime-main.aaca3b7e.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/React-JobAPI/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/React-JobAPI/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "6b5ff5a0e7f81b4c1247726c10c7aef5",
+    "url": "/React-JobAPI/static/media/bg-header-mobile.6b5ff5a0.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/React-JobAPI/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/React-JobAPI/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "ae67c678a143e6c6b913629f3d3b55e9",
+    "url": "/React-JobAPI/static/media/icon-remove.ae67c678.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/React-JobAPI/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/React-JobAPI/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/React-JobAPI/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/React-JobAPI/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/React-JobAPI/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/React-JobAPI/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/React-JobAPI/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
